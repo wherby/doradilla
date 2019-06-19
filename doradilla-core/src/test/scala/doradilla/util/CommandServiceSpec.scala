31da630ceeae4a22419603d4a2dc3f20bf6aaf11
wherby <187225577@qq.com>
2019-06-19 22:56:31 +0800

    reenable test case

--- doradilla-core/src/test/scala/doradilla/util/CommandServiceSpec.scala
+++ doradilla-core/src/test/scala/doradilla/util/CommandServiceSpec.scala
@@ -7,7 +7,7 @@ import vars.ConstVarTest
   * For doradilla.util in Doradilla
   * Created by whereby[Tao Zhou](187225577@qq.com) on 2019/4/7
   */
-/*class CommandServiceSpec extends AsyncFlatSpec{
+class CommandServiceSpec extends AsyncFlatSpec{
     "ProcessService" should  "Run a sleep 0.5 second could result succss" in {
       val resultFuture = CommandService.runCommandProcess(ConstVarTest.cmdWin, ConstVarTest.cmdLinux)
       resultFuture.map {
@@ -16,4 +16,4 @@ import vars.ConstVarTest
       }
     }
 
-}*/
+}
