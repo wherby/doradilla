42cfca7eeaeb9af74d2c23de1a996057e3dba723
stevenz33 <steven.t.zhou@cn.pwc.com>
2019-10-23 16:03:00 +0800

    release 1.6.0.2 fix blocking io pool setting

--- doradilla-core/src/main/scala/doracore/tool/job/worker/BlockIODispatcher.scala
+++ doradilla-core/src/main/scala/doracore/tool/job/worker/BlockIODispatcher.scala
@@ -1,18 +1,17 @@
 package doracore.tool.job.worker
 
-import java.util.concurrent.Executors
 
 import akka.actor.Actor
 import doracore.vars.ConstVars
 
-import scala.concurrent.ExecutionContext
+
 
 trait BlockIODispatcher {
   this:Actor =>
   def GetBlockIODispatcher = {
     context.system.dispatchers.hasDispatcher(ConstVars.blockDispatcherName) match {
       case true => context.system.dispatchers.lookup(ConstVars.blockDispatcherName)
-      case _ => ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+      case _ => context.system.dispatcher
     }
   }
 }
