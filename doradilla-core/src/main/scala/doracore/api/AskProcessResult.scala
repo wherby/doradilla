c578c248efa54188553d7ad464d2cae19d157c9f
stevenz33 <steven.t.zhou@cn.pwc.com>
2019-11-13 18:07:39 +0800

    code refactor addAskTrait

--- doradilla-core/src/main/scala/doracore/api/AskProcessResult.scala
+++ doradilla-core/src/main/scala/doracore/api/AskProcessResult.scala
@@ -1,5 +1,30 @@
 package doracore.api
 
-trait AskProcessResult {
+import akka.actor.{ActorRef, PoisonPill}
+import akka.event.slf4j.Logger
+import akka.util.Timeout
+import doracore.core.msg.Job.{JobRequest, JobResult, JobStatus}
+import doracore.tool.receive.ReceiveActor.{FetchResult, ProxyControlMsg}
 
+import scala.concurrent.{Await, ExecutionContext, Future}
+import akka.pattern.ask
+
+trait AskProcessResult {
+  def getProcessCommandFutureResult(jobRequest: JobRequest, driver:ActorRef, receiveActor: ActorRef, timeout: Timeout )(implicit ex: ExecutionContext): Future[JobResult] = {
+    driver.tell(jobRequest, receiveActor)
+    var result = JobResult(JobStatus.Unknown, "Unkonwn").asInstanceOf[Any]
+    implicit val timeoutValue: Timeout = timeout
+    try {
+      result = Await.result((receiveActor ? FetchResult()), timeout.duration)
+    } catch {
+      case ex: Throwable =>
+        Logger.apply(this.getClass.getName).error(s"$jobRequest timeout after $timeout")
+        result = JobResult(JobStatus.TimeOut, ex.toString)
+        receiveActor ! ProxyControlMsg(result)
+        Thread.sleep(100)
+    }
+    receiveActor ! ProxyControlMsg(PoisonPill)
+    receiveActor ! PoisonPill
+    Future(result.asInstanceOf[JobResult])
+  }
 }
