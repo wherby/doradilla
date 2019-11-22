f44dd75d256b3d556c1ebd333c8f655a7a029904
stevenz33 <steven.t.zhou@cn.pwc.com>
2019-11-22 13:11:08 +0800

    remove await in AskProcessResult

--- doradilla-core/src/main/scala/doracore/api/AskProcessResult.scala
+++ doradilla-core/src/main/scala/doracore/api/AskProcessResult.scala
@@ -6,7 +6,7 @@ import akka.util.Timeout
 import doracore.core.msg.Job.{JobRequest, JobResult, JobStatus}
 import doracore.tool.receive.ReceiveActor.{FetchResult, ProxyControlMsg}
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import akka.pattern.ask
 import doracore.util.ProcessService.ProcessResult
 
@@ -15,23 +15,27 @@ trait AskProcessResult {
   def getProcessCommandFutureResult(jobRequest: JobRequest, driver:ActorRef, receiveActor: ActorRef, timeout: Timeout ): Future[JobResult] = {
     driver.tell(jobRequest, receiveActor)
     var result = JobResult(JobStatus.Unknown, "Unkonwn").asInstanceOf[Any]
+    implicit val ex: ExecutionContext = getBlockDispatcher()
 
-    def getResult = {
+    def getResult:Future[JobResult] = {
       implicit val timeoutValue: Timeout = timeout
-      try {
-        result = Await.result((receiveActor ? FetchResult()), timeout.duration)
-      } catch {
+      (receiveActor ? FetchResult()).map{
+        resultT => resultT.asInstanceOf[JobResult]
+      }.recover{
         case ex: Throwable =>
           val tName = Thread.currentThread.getName
           Logger.apply(this.getClass.getName).error(s"$tName=> $jobRequest timeout after $timeout")
           result = JobResult(JobStatus.TimeOut, ProcessResult(JobStatus.Failed, ex))
           receiveActor ! ProxyControlMsg(result)
           Thread.sleep(100)
+          result.asInstanceOf[JobResult]
+      }.map{
+        result=>
+        receiveActor ! ProxyControlMsg(PoisonPill)
+        receiveActor ! PoisonPill
+        result
       }
-      receiveActor ! ProxyControlMsg(PoisonPill)
-      receiveActor ! PoisonPill
-      result.asInstanceOf[JobResult]
     }
-    Future(getResult)(getBlockDispatcher())
+    getResult
   }
 }
